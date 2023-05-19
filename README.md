d48b28fc1bb65ad658fff4152516b2fafdcccdc7
pushpraj-rmx <akshaydwivedi19@gmail.com>
2023-05-19 13:00:19 +0530

    added starter files and some updates

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # JavaScript
+
+
+01 : Welocme note, Course Materials, links 
+02 : Fundamentals - Part 01
+03 : Fundamentals - Part 02
+04 : How to navigate this  course 
+05 : Developer Skills
+06 : 
+07 : DOM Manipulation 
+08 : How JavaScript Works 
+09 : Modern Operators
+10 : Functions 
+11 : Arrays 
+12 : Numbers , Dates , Timers 
+13 : Advanced DOM
+14 : Object-Oriented JS 
+15 : MAPTY Projects 
+16 : Asynchronous JS 
+17 : Modern JS Applications
+18 : Forkify Project
+19 : Deployment and Git
---
+++ assets/js/js.fundamentals.js
@@ -0,0 +1,66 @@
+// declarration 
+
+/*
+1. let
+2. const
+3. var
+*/
+let age = 30;
+const intro = "Hello everyone";
+console.log(age)
+age = 32 
+console.log(intro + 'now I am ' + age  )
+// 
+/*
+Datatypes
+values are either object or primitive 
+
+# primitive datatypes 
+
+1. Numbers are always floating point 
+2. Strings - sequence of characters  # firstName = yourName
+3. Boolean
+4. Undefined - declared but value not set 
+5. Null - 
+6. Symbol - 
+7. BigInt 
+
+*/
+
+
+let x = 'value' ;
+// later you can change its value by 
+x = 'newValue'
+
+
+console.log(x)
+
+
+console.log(typeof(true))
+
+// no quotes = variables
+
+// always use const ,
+// if you sure 
+
+// let is block scoped
+//var is function scoped
+// we will lean more later 
+
+// ******************************************************
+
+/*########|OPERTORSS|####
+
+// mathematical/arithemetic  operators
+
++ = plus
+- = minus
+* = multiply
+/ = divide
+% = modulus/remainer 
+** = exponential operator 
+
+
+*/
+
+
